fb5c8ec47084f93f09f668b13581bf481c998d9b
unknown <iabhideshmukhis@gmail.com>
2022-06-27 21:16:14 +0200

    Update features.py

--- preprocessing/features.py
+++ preprocessing/features.py
@@ -48,9 +48,8 @@ def rri_std(signals, fs):
 
 def extract_r_p_peaks(sig, fs):
     _, rpeaks = nk.ecg_peaks(sig, sampling_rate=fs)
-    rpeaks['ECG_R_Peaks'] = rpeaks['ECG_R_Peaks'][0:-1]  #Avoid indexing overflow error
-    
-    _, waves_peak = nk.ecg_delineate(sig, rpeaks, sampling_rate=fs, method="dwt")
+    rpeaks['ECG_R_Peaks'] = rpeaks['ECG_R_Peaks'][0:-1]  #Avoid indexing overflow error, Need to find robust method later
+    _, waves_peak = nk.ecg_delineate(sig, rpeaks, sampling_rate=fs, method="cwt")
     r_peaks = rpeaks['ECG_R_Peaks']
     p_peaks = waves_peak['ECG_P_Peaks']
     #print(len(p_peaks))
@@ -59,18 +58,20 @@ def extract_r_p_peaks(sig, fs):
     return r_peaks, p_peaks  #return sample points of R and P peaks   
 
 def poincare_sd(r_peaks):   #input argument r_peaks in samples only
-    r_peaks = r_peaks*10/3  #Convert r_peaks from samples to time of occurence in millisec 
-    rr = np.diff(r_peaks)
-    rr_n = rr[:-1]
-    rr_n1 = rr[1:]  #shifted rr duration
-    sd1 = np.sqrt(0.5) * np.std(rr_n1 - rr_n)
-    sd2 = np.sqrt(0.5) * np.std(rr_n1 + rr_n)
+    try: 
+        r_peaks = r_peaks*10/3  #Convert r_peaks from samples to time of occurence in millisec 
+        rr = np.diff(r_peaks)
+        rr_n = rr[:-1]
+        rr_n1 = rr[1:]  #shifted rr duration
+        sd1 = np.sqrt(0.5) * np.std(rr_n1 - rr_n)
+        sd2 = np.sqrt(0.5) * np.std(rr_n1 + rr_n)
+    except:
+        sd1, sd2 = 0, 0
     return sd1, sd2, sd1/sd2    #return sd1, sd2 and their ratio
 
 def stats_features(points):
-    points = np.array(points)
-    mean, std, q2, iqr, quartile_coeff_disp = 0, 0, 0, 0, 0
-    if len(points) != 0: 
+    try: 
+        points = np.array(points)
         mean = points.mean()
         std = points.std()
         q2 = np.quantile(points, 0.5)
@@ -78,53 +79,64 @@ def stats_features(points):
         q3 = np.quantile(points, 0.75)
         iqr = q3 - q1
         quartile_coeff_disp = (q3 - q1)/ (q3 + q1)
+        
+    except:
+        mean, std, q2, iqr, quartile_coeff_disp = 0, 0, 0, 0, 0
     
     return mean, std, q2, iqr, quartile_coeff_disp
 
 def dominant_freq(sig):
-    f, Pxx_den = signal.periodogram(sig, 300)
-    max_y = max(Pxx_den)    # Find the maximum y value
-    max_x = f[Pxx_den.argmax()]  # Find the x value corresponding to the maximum y value
-    energy_percentile = max_y/Pxx_den.sum()
+    try: 
+        f, Pxx_den = signal.periodogram(sig, 300)
+        max_y = max(Pxx_den)    # Find the maximum y value
+        max_x = f[Pxx_den.argmax()]  # Find the x value corresponding to the maximum y value
+        energy_percentile = max_y/Pxx_den.sum()
+    except: 
+        max_x, energy_percentile = 0, 0
     return max_x, energy_percentile  #returns dominant frequency in Hertz
 
 def beats_per_sec(sig, rpeaks):
-    duration_sec = len(sig)*(1/300)  #Duration of signal in seconds
-    #beats = len(r_peaks)
-    beats_per_sec = rpeaks/ duration_sec
+    try: 
+        duration_sec = len(sig)*(1/300)  #Duration of signal in seconds
+        #beats = len(r_peaks)
+        beats_per_sec = rpeaks/ duration_sec
+    except: 
+        beats_per_sec = 0  
     return beats_per_sec   #Generally Normal Beats_per_sec: 1 to 1.67, (Not always followed)    
 
 def distribution_score(r_peaks, sig):
-    first_peak = r_peaks[0]
-    last_peak = r_peaks[-1]
-    sig = sig[first_peak: last_peak+1]
-    samples_per_beat = len(sig)/ (len(r_peaks) - 1)
-    #Set threshold, Hardcoded for now, Need to automate later
-    lower_thresh = samples_per_beat - 30    
-    higher_thresh = samples_per_beat + 30
-    cumdiff = np.diff(r_peaks) 
-    #Number of R-R durations lie inside threshold
-    len_center_idx = len([i for i in cumdiff if i > lower_thresh and i < higher_thresh])
-    score1 =  len_center_idx/ len(cumdiff)
-    ##################################################################################################
-    max1 = np.quantile(cumdiff, 0.5)
-    #max1 = x[np.argmax(y)]
-    #print(max1)
-    lower_thresh = max1 - 30
-    higher_thresh = max1 + 30
-    #Number of R-R durations lie inside threshold
-    len_center_idx = len([i for i in cumdiff if i > lower_thresh and i < higher_thresh])
-    score2 =  len_center_idx/ len(cumdiff)
-    ##################################################################################################
-    std = np.std(cumdiff) 
-    lower_thresh = max1 - std
-    higher_thresh = max1 + std
-    #Number of R-R durations lie inside threshold
-    len_center_idx = len([i for i in cumdiff if i > lower_thresh and i < higher_thresh])
-    score3 =  len_center_idx/ len(cumdiff)
-    ###################################################################################################
+    try: 
+        first_peak = r_peaks[0]
+        last_peak = r_peaks[-1]
+        sig = sig[first_peak: last_peak+1]
+        samples_per_beat = len(sig)/ (len(r_peaks) - 1)
+        #Set threshold, Hardcoded for now, Need to automate later
+        lower_thresh = samples_per_beat - 30    
+        higher_thresh = samples_per_beat + 30
+        cumdiff = np.diff(r_peaks) 
+        #Number of R-R durations lie inside threshold
+        len_center_idx = len([i for i in cumdiff if i > lower_thresh and i < higher_thresh])
+        score1 =  len_center_idx/ len(cumdiff)
+        ##################################################################################################
+        max1 = np.quantile(cumdiff, 0.5)
+        #max1 = x[np.argmax(y)]
+        #print(max1)
+        lower_thresh = max1 - 30
+        higher_thresh = max1 + 30
+        #Number of R-R durations lie inside threshold
+        len_center_idx = len([i for i in cumdiff if i > lower_thresh and i < higher_thresh])
+        score2 =  len_center_idx/ len(cumdiff)
+        ##################################################################################################
+        std = np.std(cumdiff) 
+        lower_thresh = max1 - std
+        higher_thresh = max1 + std
+        #Number of R-R durations lie inside threshold
+        len_center_idx = len([i for i in cumdiff if i > lower_thresh and i < higher_thresh])
+        score3 =  len_center_idx/ len(cumdiff)
+        ###################################################################################################
+    except:
+        score1, score2, score3 = 0, 0, 0
     return score1, score2, score3
-
 
 def compute_FFT(sig, fs=300):
     spectrum = fftpack.fft(sig)
